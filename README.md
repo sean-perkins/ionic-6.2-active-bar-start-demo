fedba8e0437d3e6443ced75ba401c603039c7363
Sean Perkins <sean@ionic.io>
2022-07-21 16:58:58 -0400

    chore(): demo

---
+++ README.md
@@ -0,0 +1,3 @@
+Demo of using `ion-range` with `activeBarStart` in Ionic 6.2.0.
+
+<video src="https://user-images.githubusercontent.com/13732623/180314122-2daf9e03-613e-4f25-9ed7-ed535b073c40.mp4"></video>
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  value = 0;
+
+  emoji = '';
+
+  ngOnInit() {
+    this.setEmoji();
+  }
+
+  onRangeChange(newValue: number) {
+    this.value = newValue;
+    this.setEmoji();
+  }
+
+  pinFormatter(value) {
+    return `${value} C\u00B0`;
+  }
+
+  private setEmoji() {
+    if (this.value > 50) {
+      this.emoji = '☠️';
+    } else if (this.value > 30) {
+      this.emoji = '🥵';
+    } else if (this.value > 0) {
+      this.emoji = '😶';
+    } else if (this.value < -50) {
+      this.emoji = '🧊';
+    } else if (this.value <= 0) {
+      this.emoji = '🥶';
+    }
+  }
 
 }
