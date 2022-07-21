9dddb1eb87f53a2bbb70b41c882282a56d76110c
Sean Perkins <sean@ionic.io>
2022-07-21 16:16:15 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-6.2-active-bar-start-demo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
